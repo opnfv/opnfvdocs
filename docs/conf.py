cabc633a680f8617815ae1e4bc273c040616852c
Trevor Bramwell <tbramwell@linuxfoundation.org>
2018-11-13 11:12:10 -0800

    Set version/release to Gambia
    Change-Id: Ib9cbadc90f35302a682612d2ddc79aa7b6ce2e71
    Signed-off-by: Trevor Bramwell <tbramwell@linuxfoundation.org>

--- docs/conf.py
+++ docs/conf.py
@@ -42,9 +42,9 @@ author = 'Open Platform for NFV'
 # built documents.
 #
 # The short X.Y version.
-version = 'Latest'
+version = 'Gambia'
 # The full version, including alpha/beta/rc tags.
-release = 'Latest'
+release = 'Gambia'
 # The branch this project should link against for projects
 # following the stable-branch process. On the opnfvdocs stable branch
 # this should be set to 'stable-<release>' where '<release>' is the
