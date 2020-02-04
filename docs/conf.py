7412e47c547efc30fa65b2d07ce0ed201e0b20d2
Sofia Wallin <sofia.wallin@est.tech>
2020-02-04 15:36:34 +0100

    Set version/release to Iruya
    Change-Id: If2262daf612d7890dfa99983072f14f7afd7376e
    Signed-off-by: Sofia Wallin <sofia.wallin@est.tech>

--- docs/conf.py
+++ docs/conf.py
@@ -42,9 +42,9 @@ author = 'Open Platform for NFV'
 # built documents.
 #
 # The short X.Y version.
-version = 'Latest'
+version = 'Iruya'
 # The full version, including alpha/beta/rc tags.
-release = 'Latest'
+release = 'Iruya'
 # The branch this project should link against for projects
 # following the stable-branch process. On the opnfvdocs stable branch
 # this should be set to 'stable/<release>' where '<release>' is the
